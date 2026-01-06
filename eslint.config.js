a19931a5b2bab034d3f44a8bb6c90aabfd06d5b4
UchiaLord <wychert97@live.at>
2026-01-06 11:14:01 +0100

    chore(api): entferne unnoetiges eslint-disable und finalisiere lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,20 @@
 import js from '@eslint/js';
 import prettier from 'eslint-config-prettier';
 import importPlugin from 'eslint-plugin-import';
+import globals from 'globals';
 
 export default [
   js.configs.recommended,
+  prettier,
   {
     plugins: { import: importPlugin },
+    languageOptions: {
+      globals: {
+        ...globals.node
+      }
+    },
     rules: {
-      'import/order': [
-        'warn',
-        { 'newlines-between': 'always', alphabetize: { order: 'asc' } }
-      ]
+      'import/order': ['warn', { 'newlines-between': 'always' }]
     }
-  },
-  prettier
+  }
 ];
